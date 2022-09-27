44f6123a9f05c50f5d18b3cb1d36c4507b7a050e
revdev <munkherdene.rev@gmail.com>
2022-09-27 13:40:45 +0800

    default v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,11 @@ import React from "react";
 import '../styles/globals.css'
 import 'antd/dist/antd.css'
 import { AppProps } from 'next/app'
-import { BannerStore } from "../context/BannerContext";
-import { RedeemStore } from "../context/RedeemContext";
-import { PartnerStore } from "../context/PartnerContext";
 
 import { 
   Layout
 } from "antd";
 
-import { BkoMenu } from "../components/Layout/BkoMenu";
-import BkoSocials from "../components/Layout/BkoSocials";
-import BkoContact from "../components/Layout/BkoContact";
 import Head from "next/head";
 
 const { Header, Content, Footer } = Layout;
@@ -27,45 +21,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet" />
       </Head>
-      <BannerStore>
-        <PartnerStore>
-          <RedeemStore>
-            <Layout>
-              {/* <Header className='bg-white'>
-                <BkoMenu />
-              </Header> */}
-              <Content className="bg-slate-50">
-                <div 
-                  className=""
-                  style={{
-                    
-                    backgroundColor: "#FAFAFC",
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center",
-                    backgroundImage: "url(https://res.cloudinary.com/daliec2ib/image/upload/v1659533862/BKO/assets/Blend_Group_2_zaauz1.png)"
-                  }}
-                >
-                  <Component {...pageProps} />
-                </div>
-              </Content>
-              <Footer>
-                <div className='container mx-auto'>
-                  <div className="grid grid-cols-6 md:mx-0">
-                    <div className="md:col-span-3 col-span-1 mr-auto"> 
-                      <BkoSocials />
-                      {/* <img src='https://res.cloudinary.com/daliec2ib/image/upload/v1658147950/BKO/assets/273269846_956798281707222_3269984056906119710_n_gccix1.png' width="50px" height="auto"/> */}
-                    </div>
-                    <div className="md:col-span-3 col-span-5 ml-auto">
-                      <BkoContact />
-                    </div>  
-                  </div>
-                </div>
-              </Footer>
-            </Layout>
-          </RedeemStore>
-        </PartnerStore>
-      </BannerStore>
+      <Layout>
+        <Content className="bg-slate-50">
+          
+          <Component {...pageProps} />
+        </Content>
+      </Layout>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,10 @@
 import React, { useContext, useEffect } from 'react';
 import Head from "next/head";
 
-import RedeemContext from "../context/RedeemContext";
-import BannerContext from "../context/BannerContext";
-import PartnerContext from '../context/PartnerContext';
+export default function HomePage() {
 
-import { RedeemHeader } from "../components/Layout/RedeemHeader";
-import { BkoTerms } from "../components/Bko/Terms";
-// import { RedeemForm } from "../components/Bko/RedeemForm";
-import RedeemForm from "../components/Bko/RedeemFormNew";
-import BkoGrid from "../components/Layout/BkoGrid";
-
-export default function RedeemPage() {
-
-  const context = useContext(RedeemContext);
-  const bannerContext = useContext(BannerContext);
-  const partnerContext = useContext(PartnerContext);
-
-  const { checkRedeem } = context;
   
   useEffect(() => {
-    bannerContext.getBanners();
-    partnerContext.getPartners();
   }, []);
 
   // console.log("BANNER: ", bannerContext.state)
@@ -36,12 +19,7 @@ export default function RedeemPage() {
           href="https://cdn-icons-png.flaticon.com/512/3202/3202926.png"
         /> */}
       </Head>
-      <RedeemHeader data={bannerContext.state.list}>
-        {/* <RedeemForm onFinish={checkRedeem} /> */}
-        <RedeemForm onFinish={checkRedeem} />
-      </RedeemHeader>
-      <BkoGrid data={partnerContext.state.list}/> 
-      <BkoTerms />
+      hi
     </div>
   )
 }
